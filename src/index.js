f883283b612908a246c294ce3a8faa6725132e96
Luis Ortiz <luis.ortiz7905@gmail.com>
2023-11-20 17:45:53 -0400

    Done Solved lab

--- src/index.js
+++ src/index.js
@@ -28,6 +28,8 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
+  memoryGame.shuffleCards();
+
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -41,11 +43,53 @@ window.addEventListener('load', (event) => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+  let clickedSpan = document.getElementById('pairs-clicked')
+  let guessedSpan = document.getElementById('pairs-guessed')
+
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      if(memoryGame.pickedCards.length < 2){
+        card.classList.toggle('turned');
+        memoryGame.pickedCards.push(card);
+
+        if(memoryGame.pickedCards.length === 2){
+          let card1 = memoryGame.pickedCards[0].getAttribute('data-card-name');
+          let card2 = memoryGame.pickedCards[1].getAttribute('data-card-name');
+
+          if(memoryGame.checkIfPair(card1, card2) === true){
+            clickedSpan.innerText = `${memoryGame.pairsClicked}`;
+            guessedSpan.innerText = `${memoryGame.pairsGuessed}`;
+
+            memoryGame.pickedCards = [];
+          
+            if(memoryGame.checkIfFinished() === true){
+              setTimeout(() => {
+              alert("Congratulations! You've won!");
+              }, 1000)
+            }
+          }
+        }else{
+          clickedSpan.innerText = `${memoryGame.pairsClicked}`;
+          setTimeout(() => {
+            memoryGame.pickedCards[0].classList.toggle('turned');
+            memoryGame.pickedCards[1].classList.toggle('turned');
+            memoryGame.pickedCards = [];
+          }, 1000)
+        }
+      }
+
+      //console.log(`Card clicked: ${card}`);
     });
   });
 });
+
+
+// add card to pickedCards array
+// only turn if you have less than 2 cards in the pickedCards
+// when you have two, checkIfPair
+//if checkIfPair === true, cards remain flipped and checkIfFinished
+//otherwise flip back
+//
--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,46 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
     // ... write your code here
+    if(!this.cards){
+      return undefined;
+    }else{
+      for(let i = this.cards.length - 1 ; i > 0; i--) {
+        let random = Math.floor(Math.random() * (i + 1));
+        let temp = this.cards[i];
+        this.cards[i] = this.cards[random];
+        this.cards[random] = temp;
+      }
+    }
+    
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++;
+
+    if(card1 === card2){
+      this.pairsGuessed++;
+      return true;
+    }
+    
+    return false;
   }
 
   checkIfFinished() {
     // ... write your code here
+    if(this.pairsGuessed === 0){
+      return false;
+    }else if(this.pairsGuessed !== 12){
+      return false;
+    }else{
+      return true;
+    }
   }
 }
